2fb29b1050e1b9b234dc1c07cbdf639eb55c12c3
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2020-11-13 15:36:50 +0100

    First yarp device publishing on ROS 2 prototype

--- src/devices/ros2test/Ros2Test.cpp
+++ src/devices/ros2test/Ros2Test.cpp
@@ -8,13 +8,48 @@
 
 #include "Ros2Test.h"
 
-bool open(yarp::os::Searchable& config)
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include <yarp/os/LogComponent.h>
+#include <yarp/os/LogStream.h>
+
+using namespace std::chrono_literals;
+
+YARP_LOG_COMPONENT(ROS2TEST, "yarp.ros2.ros2test");
+
+MinimalPublisher::MinimalPublisher() :
+        Node("ros2test_node"), count_(0)
 {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("ros2test_topic", 10);
+    timer_ = this->create_wall_timer(
+        500ms,
+        std::bind(&MinimalPublisher::timer_callback, this)
+    );
+}
+
+void MinimalPublisher::timer_callback()
+{
+    auto message = std_msgs::msg::String();
+    message.data = "Hello, world! " + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+}
+
+bool Ros2Test::open(yarp::os::Searchable& config)
+{
+    yCInfo(ROS2TEST, "Ros2Test::open");
     YARP_UNUSED(config);
+    rclcpp::init(/*argc*/ 0, /*argv*/ nullptr);
+    m_pub = std::make_shared<MinimalPublisher>();
+    rclcpp::spin(m_pub);
     return true;
 }
 
-bool close()
+bool Ros2Test::close()
 {
+    yCInfo(ROS2TEST, "Ros2Test::close");
     return true;
 }
--- src/devices/ros2test/Ros2Test.h
+++ src/devices/ros2test/Ros2Test.h
@@ -11,6 +11,24 @@
 
 #include <yarp/dev/DeviceDriver.h>
 
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+class MinimalPublisher :
+    public rclcpp::Node
+{
+public:
+    MinimalPublisher();
+
+  private:
+    void timer_callback();
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
+  };
+
+
 class Ros2Test :
         public yarp::dev::DeviceDriver
 {
@@ -25,6 +43,9 @@ public:
     // DeviceDriver
     bool open(yarp::os::Searchable& config) override;
     bool close() override;
+
+private:
+    std::shared_ptr<MinimalPublisher> m_pub;
 };
 
 #endif // YARP_ROS2_ROS2TEST_H
