a31ca9bb2b35a60a1ab912361b55b03fb378902b
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2021-06-23 19:11:50 +0200

    Add frameGrabber_nws_ros2 device

---
+++ src/devices/frameGrabber_nws_ros2/frameGrabber_nws_ros2.cpp
@@ -0,0 +1,381 @@
+/*
+ * Copyright (C) 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * Copyright (C) 2006-2010 RobotCub Consortium
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#include "frameGrabber_nws_ros2.h"
+
+#include <yarp/os/LogComponent.h>
+#include <yarp/os/LogStream.h>
+#include <yarp/dev/PolyDriver.h>
+
+#include <sensor_msgs/image_encodings.hpp>
+
+namespace {
+YARP_LOG_COMPONENT(FRAMEGRABBER_NWS_ROS2, "yarp.device.frameGrabber_nws_ros2")
+
+// FIXME Copied from rgbdSensor_nws_ros2
+std::string yarp2RosPixelCode(int code)
+{
+    switch (code)
+    {
+    case VOCAB_PIXEL_BGR:
+        return sensor_msgs::image_encodings::BGR8;
+    case VOCAB_PIXEL_BGRA:
+        return sensor_msgs::image_encodings::BGRA8;
+    case VOCAB_PIXEL_ENCODING_BAYER_BGGR16:
+        return sensor_msgs::image_encodings::BAYER_BGGR16;
+    case VOCAB_PIXEL_ENCODING_BAYER_BGGR8:
+        return sensor_msgs::image_encodings::BAYER_BGGR8;
+    case VOCAB_PIXEL_ENCODING_BAYER_GBRG16:
+        return sensor_msgs::image_encodings::BAYER_GBRG16;
+    case VOCAB_PIXEL_ENCODING_BAYER_GBRG8:
+        return sensor_msgs::image_encodings::BAYER_GBRG8;
+    case VOCAB_PIXEL_ENCODING_BAYER_GRBG16:
+        return sensor_msgs::image_encodings::BAYER_GRBG16;
+    case VOCAB_PIXEL_ENCODING_BAYER_GRBG8:
+        return sensor_msgs::image_encodings::BAYER_GRBG8;
+    case VOCAB_PIXEL_ENCODING_BAYER_RGGB16:
+        return sensor_msgs::image_encodings::BAYER_RGGB16;
+    case VOCAB_PIXEL_ENCODING_BAYER_RGGB8:
+        return sensor_msgs::image_encodings::BAYER_RGGB8;
+    case VOCAB_PIXEL_MONO:
+        return sensor_msgs::image_encodings::MONO8;
+    case VOCAB_PIXEL_MONO16:
+        return sensor_msgs::image_encodings::MONO16;
+    case VOCAB_PIXEL_RGB:
+        return sensor_msgs::image_encodings::RGB8;
+    case VOCAB_PIXEL_RGBA:
+        return sensor_msgs::image_encodings::RGBA8;
+    case VOCAB_PIXEL_MONO_FLOAT:
+        return sensor_msgs::image_encodings::TYPE_32FC1;
+    default:
+        return sensor_msgs::image_encodings::RGB8;
+    }
+}
+} // namespace
+
+
+
+
+Ros2Init::Ros2Init()
+{
+    rclcpp::init(/*argc*/ 0, /*argv*/ nullptr);
+    node = std::make_shared<rclcpp::Node>("yarprobotinterface_node");
+}
+
+Ros2Init& Ros2Init::get()
+{
+    static Ros2Init instance;
+    return instance;
+}
+
+
+
+FrameGrabber_nws_ros2::FrameGrabber_nws_ros2() :
+        PeriodicThread(s_default_period)
+{
+}
+
+
+FrameGrabber_nws_ros2::~FrameGrabber_nws_ros2()
+{
+    close();
+}
+
+
+bool FrameGrabber_nws_ros2::close()
+{
+    if (!m_active) {
+        return false;
+    }
+    m_active = false;
+
+    detach();
+
+    if (node != nullptr) {
+        node->interrupt();
+        delete node;
+        node = nullptr;
+    }
+
+    if (subdevice) {
+        subdevice->close();
+        delete subdevice;
+        subdevice = nullptr;
+    }
+
+    isSubdeviceOwned = false;
+
+    return true;
+}
+
+
+bool FrameGrabber_nws_ros2::open(yarp::os::Searchable& config)
+{
+    if (m_active) {
+        yCError(FRAMEGRABBER_NWS_ROS2, "Device is already opened");
+        return false;
+    }
+
+
+    // Check "period" option
+    if (config.check("period", "refresh period(in s) of the broadcasted values through yarp ports") && config.find("period").isFloat64()) {
+        m_period = config.find("period").asFloat64();
+    } else {
+        yCInfo(FRAMEGRABBER_NWS_ROS2)
+            << "Period parameter not found, using default of"
+            << s_default_period
+            << "seconds";
+    }
+    PeriodicThread::setPeriod(m_period);
+
+
+    // Check "nodeName" option and open node
+    std::string nodeName = config.check("nodeName", yarp::os::Value("/node"), "name of node").asString();
+    node = new yarp::os::Node(nodeName);
+
+
+    // Check "imageTopicName" option and open publisher
+    std::string imageTopicName = config.check("imageTopicName", yarp::os::Value("/image"), "name of the image topic").asString();
+    publisher_image = Ros2Init::get().node->create_publisher<sensor_msgs::msg::Image>(imageTopicName, 10);
+
+
+    // Check "cameraInfoTopicName" option and open publisher
+    std::string cameraInfoTopicName = config.check("cameraInfoTopicName", yarp::os::Value("/cameraInfo"), "name of the camera info topic").asString();
+    publisher_cameraInfo = Ros2Init::get().node->create_publisher<sensor_msgs::msg::CameraInfo>(cameraInfoTopicName, 10);
+
+
+    // Check "frameId" option
+    m_frameId = config.check("frameId", yarp::os::Value("frame_id"), "name of the frame id").asString();
+
+//     nodeSeq = 0;
+
+
+    // Check "subdevice" option and eventually open the device
+    isSubdeviceOwned = config.check("subdevice");
+    if (isSubdeviceOwned) {
+        yarp::os::Property p;
+        subdevice = new yarp::dev::PolyDriver;
+        p.fromString(config.toString());
+        p.put("pixelType", VOCAB_PIXEL_RGB);
+        p.setMonitor(config.getMonitor(), "subdevice"); // pass on any monitoring
+        p.unput("device");
+        p.put("device", config.find("subdevice").asString()); // subdevice was already checked before
+
+        // if errors occurred during open, quit here.
+        subdevice->open(p);
+
+        if (!(subdevice->isValid())) {
+            yCError(FRAMEGRABBER_NWS_ROS2, "Unable to open subdevice");
+            return false;
+        }
+        if (!attach(subdevice)) {
+            yCError(FRAMEGRABBER_NWS_ROS2, "Unable to attach subdevice");
+            return false;
+        }
+    } else {
+        yCInfo(FRAMEGRABBER_NWS_ROS2) << "Running, waiting for attach...";
+    }
+
+    m_active = true;
+
+    return true;
+}
+
+bool FrameGrabber_nws_ros2::attach(yarp::dev::PolyDriver* poly)
+{
+    if (!poly->isValid()) {
+        yCError(FRAMEGRABBER_NWS_ROS2) << "Device " << poly << " to attach to is not valid ... cannot proceed";
+        return false;
+    }
+
+    poly->view(iRgbVisualParams);
+    poly->view(iFrameGrabberImage);
+    poly->view(iPreciselyTimed);
+
+    if (iFrameGrabberImage == nullptr) {
+        yCError(FRAMEGRABBER_NWS_ROS2) << "IFrameGrabberImage interface is not available on the device";
+        return false;
+    }
+
+    if (iRgbVisualParams == nullptr) {
+        yCWarning(FRAMEGRABBER_NWS_ROS2) << "IRgbVisualParams interface is not available on the device";
+    }
+
+    return PeriodicThread::start();
+}
+
+
+bool FrameGrabber_nws_ros2::detach()
+{
+    if (yarp::os::PeriodicThread::isRunning()) {
+        yarp::os::PeriodicThread::stop();
+    }
+
+    iRgbVisualParams = nullptr;
+    iFrameGrabberImage = nullptr;
+    iPreciselyTimed = nullptr;
+
+    return true;
+}
+
+bool FrameGrabber_nws_ros2::threadInit()
+{
+    yarpimg = new yarp::sig::ImageOf<yarp::sig::PixelRgb>;
+    return true;
+}
+
+void FrameGrabber_nws_ros2::threadRelease()
+{
+    delete yarpimg;
+    yarpimg = nullptr;
+}
+
+
+// Publish the images on the buffered port
+void FrameGrabber_nws_ros2::run()
+{
+//     if (false /* FIXME Can we check if there are subscribers connected in ROS2? */) {
+//         // If no subscribers are connected, do not call getImage on the interface.
+//         return;
+//     }
+
+    if (iPreciselyTimed) {
+        m_stamp = iPreciselyTimed->getLastInputStamp();
+    } else {
+        m_stamp.update(yarp::os::Time::now());
+    }
+
+    if (iFrameGrabberImage /* FIXME Can we check if there are subscribers connected in ROS2 */) {
+        iFrameGrabberImage->getImage(*yarpimg);
+        sensor_msgs::msg::Image rosimg;
+        rosimg.data.resize(yarpimg->getRawImageSize());
+        rosimg.width = yarpimg->width();
+        rosimg.height = yarpimg->height();
+        rosimg.encoding = yarp2RosPixelCode(yarpimg->getPixelCode());
+        rosimg.step = yarpimg->getRowSize();
+        rosimg.header.frame_id = m_frameId;
+//         rosimg.header.stamp.sec = static_cast<int>(m_stamp.getTime()); // FIXME
+//         rosimg.header.stamp.nanosec = static_cast<int>(1000000 * (m_stamp.getTime() - int(m_stamp.getTime()))); // FIXME
+        rosimg.is_bigendian = 0;
+        memcpy(rosimg.data.data(), yarpimg->getRawImage(), yarpimg->getRawImageSize());
+        publisher_image->publish(rosimg);
+    }
+
+    if (iRgbVisualParams /* FIXME Can we check if there are subscribers connected in ROS2 */) {
+        sensor_msgs::msg::CameraInfo cameraInfo;
+        if (setCamInfo(cameraInfo)) {
+            publisher_cameraInfo->publish(cameraInfo);
+        }
+    }
+}
+
+namespace {
+template <class T>
+struct param
+{
+    param(T& inVar, std::string inName) :
+        var(&inVar),
+        parname(std::move(inName))
+    {
+    }
+    T*              var;
+    std::string     parname;
+};
+} // namespace
+
+bool FrameGrabber_nws_ros2::setCamInfo(sensor_msgs::msg::CameraInfo& cameraInfo)
+{
+    yarp::os::Property camData;
+    if (!iRgbVisualParams->getRgbIntrinsicParam(camData)) {
+        yCErrorThreadOnce(FRAMEGRABBER_NWS_ROS2) << "Unable to get intrinsic param from rgb sensor!";
+        return false;
+    }
+
+    if (!camData.check("distortionModel")) {
+        yCWarning(FRAMEGRABBER_NWS_ROS2) << "Missing distortion model";
+        return false;
+    }
+
+    std::string distModel = camData.find("distortionModel").asString();
+    if (distModel != "plumb_bob") {
+        yCError(FRAMEGRABBER_NWS_ROS2) << "Distortion model not supported";
+        return false;
+    }
+
+    double phyF = 0.0;
+    double fx = 0.0;
+    double fy = 0.0;
+    double cx = 0.0;
+    double cy = 0.0;
+    double k1 = 0.0;
+    double k2 = 0.0;
+    double t1 = 0.0;
+    double t2 = 0.0;
+    double k3 = 0.0;
+
+    std::vector<param<double>> parVector;
+    parVector.emplace_back(phyF,"physFocalLength");
+    parVector.emplace_back(fx,"focalLengthX");
+    parVector.emplace_back(fy,"focalLengthY");
+    parVector.emplace_back(cx,"principalPointX");
+    parVector.emplace_back(cy,"principalPointY");
+    parVector.emplace_back(k1,"k1");
+    parVector.emplace_back(k2,"k2");
+    parVector.emplace_back(t1,"t1");
+    parVector.emplace_back(t2,"t2");
+    parVector.emplace_back(k3,"k3");
+
+    for(auto& par : parVector) {
+        if(!camData.check(par.parname)) {
+            yCWarning(FRAMEGRABBER_NWS_ROS2) << "Driver has not the param:" << par.parname;
+            return false;
+        }
+        *(par.var) = camData.find(par.parname).asFloat64();
+    }
+
+    cameraInfo.header.frame_id      = m_frameId;
+//     cameraInfo.header.stamp.sec     = static_cast<int>(m_stamp.getTime()); // FIXME
+//     cameraInfo.header.stamp.nanosec = static_cast<int>(1000000 * (m_stamp.getTime() - int(m_stamp.getTime()))); // FIXME
+    cameraInfo.width                = iRgbVisualParams->getRgbWidth();
+    cameraInfo.height               = iRgbVisualParams->getRgbHeight();
+    cameraInfo.distortion_model     = distModel;
+
+    cameraInfo.d.resize(5);
+    cameraInfo.d[0] = k1;
+    cameraInfo.d[1] = k2;
+    cameraInfo.d[2] = t1;
+    cameraInfo.d[3] = t2;
+    cameraInfo.d[4] = k3;
+
+    cameraInfo.k[0]  = fx;       cameraInfo.k[1] = 0;        cameraInfo.k[2] = cx;
+    cameraInfo.k[3]  = 0;        cameraInfo.k[4] = fy;       cameraInfo.k[5] = cy;
+    cameraInfo.k[6]  = 0;        cameraInfo.k[7] = 0;        cameraInfo.k[8] = 1;
+
+    /*
+     * ROS documentation on cameraInfo message:
+     * "Rectification matrix (stereo cameras only)
+     * A rotation matrix aligning the camera coordinate system to the ideal
+     * stereo image plane so that epipolar lines in both stereo images are
+     * parallel."
+     * useless in our case, it will be an identity matrix
+     */
+
+    cameraInfo.r[0]  = 1;        cameraInfo.r[1] = 0;        cameraInfo.r[2] = 0;
+    cameraInfo.r[3]  = 0;        cameraInfo.r[4] = 1;        cameraInfo.r[5] = 0;
+    cameraInfo.r[6]  = 0;        cameraInfo.r[7] = 0;        cameraInfo.r[8] = 1;
+
+    cameraInfo.p[0]  = fx;      cameraInfo.p[1] = 0;    cameraInfo.p[2]  = cx;  cameraInfo.p[3]  = 0;
+    cameraInfo.p[4]  = 0;       cameraInfo.p[5] = fy;   cameraInfo.p[6]  = cy;  cameraInfo.p[7]  = 0;
+    cameraInfo.p[8]  = 0;       cameraInfo.p[9] = 0;    cameraInfo.p[10] = 1;   cameraInfo.p[11] = 0;
+
+    cameraInfo.binning_x  = cameraInfo.binning_y = 0;
+    cameraInfo.roi.height = cameraInfo.roi.width = cameraInfo.roi.x_offset = cameraInfo.roi.y_offset = 0;
+    cameraInfo.roi.do_rectify = false;
+    return true;
+}
---
+++ src/devices/frameGrabber_nws_ros2/frameGrabber_nws_ros2.h
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef YARP_FRAMEGRABBER_NWS_ROS2_H
+#define YARP_FRAMEGRABBER_NWS_ROS2_H
+
+#include <yarp/os/Node.h>
+#include <yarp/os/Publisher.h>
+#include <yarp/os/PeriodicThread.h>
+#include <yarp/os/RpcServer.h>
+#include <yarp/os/Stamp.h>
+
+#include <yarp/dev/DeviceDriver.h>
+#include <yarp/dev/IFrameGrabberControls.h>
+#include <yarp/dev/IFrameGrabberControlsDC1394.h>
+#include <yarp/dev/IFrameGrabberImage.h>
+#include <yarp/dev/IPreciselyTimed.h>
+#include <yarp/dev/IRgbVisualParams.h>
+#include <yarp/dev/WrapperSingle.h>
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
+
+class Ros2Init
+{
+public:
+    Ros2Init();
+
+    std::shared_ptr<rclcpp::Node> node;
+
+    static Ros2Init& get();
+};
+
+class FrameGrabber_nws_ros2 :
+        public yarp::dev::DeviceDriver,
+        public yarp::dev::WrapperSingle,
+        public yarp::os::PeriodicThread
+{
+private:
+    // Publishers
+    typedef rclcpp::Publisher<sensor_msgs::msg::Image> ImageTopicType;
+    typedef rclcpp::Publisher<sensor_msgs::msg::CameraInfo> CameraInfoTopicType;
+
+    yarp::os::Node* node {nullptr};
+    ImageTopicType::SharedPtr publisher_image;
+    CameraInfoTopicType::SharedPtr publisher_cameraInfo;
+
+    // Subdevice
+    yarp::dev::PolyDriver* subdevice {nullptr};
+    bool isSubdeviceOwned {false};
+
+    // Interfaces handled
+    yarp::dev::IRgbVisualParams* iRgbVisualParams {nullptr};
+    yarp::dev::IFrameGrabberImage* iFrameGrabberImage {nullptr};
+    yarp::dev::IPreciselyTimed* iPreciselyTimed {nullptr};
+
+    // Images
+    yarp::sig::ImageOf<yarp::sig::PixelRgb>* yarpimg {nullptr};
+
+    // Internal state
+    bool m_active {false};
+    yarp::os::Stamp m_stamp;
+    std::string m_frameId;
+
+    // Options
+    static constexpr double s_default_period = 0.03; // seconds
+    double m_period {s_default_period};
+
+    bool setCamInfo(sensor_msgs::msg::CameraInfo& cameraInfo);
+
+public:
+    FrameGrabber_nws_ros2();
+    FrameGrabber_nws_ros2(const FrameGrabber_nws_ros2&) = delete;
+    FrameGrabber_nws_ros2(FrameGrabber_nws_ros2&&) = delete;
+    FrameGrabber_nws_ros2& operator=(const FrameGrabber_nws_ros2&) = delete;
+    FrameGrabber_nws_ros2& operator=(FrameGrabber_nws_ros2&&) = delete;
+    ~FrameGrabber_nws_ros2() override;
+
+    // DeviceDriver
+    bool close() override;
+    bool open(yarp::os::Searchable& config) override;
+
+    // IWrapper interface
+    bool attach(yarp::dev::PolyDriver* poly) override;
+    bool detach() override;
+
+    //RateThread
+    bool threadInit() override;
+    void threadRelease() override;
+    void run() override;
+};
+
+#endif // YARP_FRAMEGRABBER_NWS_ROS2_H
