53c0f20efb611f9e5072f0d36b20172684f57fd3
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2022-01-21 11:55:30 +0100

    updated frame transform container to follow changes in yarp

--- src/devices/frameTransformUtils/FrameTransformContainer.cpp
+++ src/devices/frameTransformUtils/FrameTransformContainer.cpp
@@ -1,13 +1,14 @@
 /*
- * SPDX-FileCopyrightText: 2006-2021 Istituto Italiano di Tecnologia (IIT)
- * SPDX-License-Identifier: LGPL-2.1-or-later
- */
+* SPDX-FileCopyrightText: 2006-2021 Istituto Italiano di Tecnologia (IIT)
+* SPDX-License-Identifier: LGPL-2.1-or-later
+*/
 
 #include "FrameTransformContainer.h"
 
 #include <yarp/os/Log.h>
 #include <yarp/os/LogComponent.h>
 #include <yarp/os/LogStream.h>
+#include <algorithm>
 
 using namespace yarp::dev;
 using namespace yarp::os;
@@ -15,161 +16,181 @@ using namespace yarp::sig;
 using namespace yarp::math;
 
 namespace {
-YARP_LOG_COMPONENT(FRAMETRANSFORSTORAGE, "yarp.device.frameTransformUtils")
+YARP_LOG_COMPONENT(FRAMETRANSFORMCONTAINER, "yarp.device.frameTransformContainer")
+
 }
 
 //------------------------------------------------------------------------------------------------------------------------------
 
+void FrameTransformContainer::invalidateTransform(yarp::math::FrameTransform& trf)
+{
+   trf.timestamp = yarp::os::Time::now();
+   trf.isStatic = false;
+   trf.translation = { 0,0,0 };
+   trf.rotation = { 0,0,0,0 };
+   if (m_verbose_debug)
+   {
+       yCIDebug(FRAMETRANSFORMCONTAINER, m_name) << "At time" << std::to_string(trf.timestamp)
+                                                 << "deleted transform (marked invalid):" << trf.src_frame_id << "->" << trf.dst_frame_id << ", assigned timestamp" << std::to_string(trf.timestamp);
+   }
+}
+
 bool FrameTransformContainer::getTransforms(std::vector<yarp::math::FrameTransform>& transforms) const
 {
-    std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
-    transforms = m_transforms;
-    return true;
+   std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
+   std::copy_if (m_transforms.begin(), m_transforms.end(), std::back_inserter(transforms), [](const yarp::math::FrameTransform& trf){return trf.isValid();});
+   return true;
 }
 
 bool FrameTransformContainer::setTransforms(const std::vector<yarp::math::FrameTransform>& transforms)
 {
-    for (auto& it : transforms)
-    {
-        setTransform(it);
-    }
-    return true;
+   for (auto& it : transforms)
+   {
+       setTransform(it);
+   }
+   return true;
 }
 
-bool FrameTransformContainer::setTransform(const yarp::math::FrameTransform& t)
+bool FrameTransformContainer::setTransform(const yarp::math::FrameTransform& new_tr)
 {
-    std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
-    for (auto& it : m_transforms)
-    {
-        //this linear search may require some optimization
-        if (it.dst_frame_id == t.dst_frame_id && it.src_frame_id == t.src_frame_id)
-        {
-            //transform already exists, update it
-            it = t;
-            return true;
-        }
-    }
+   if (new_tr.isValid()==false) return true;
 
-    //add a new transform
-    m_transforms.push_back(t);
-    return true;
+   std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
+   for (auto& it : m_transforms)
+   {
+       //this linear search may require some optimization
+       if (it.dst_frame_id == new_tr.dst_frame_id && it.src_frame_id == new_tr.src_frame_id)
+       {
+           //if transform already exists and
+           //its timestamp is more recent than the currently stored transform
+           //than update it
+           if (it.isStatic == false)
+           {
+               if (new_tr.timestamp > it.timestamp)
+               {
+                   it = new_tr;
+                   return true;
+               }
+               else
+               {
+                   //yCDebug(FRAMETRANSFORMCONTAINER) << "Received old transform" << it.dst_frame_id << it.src_frame_id << std::to_string(it.timestamp) << it.isStatic;
+                   return true;
+               }
+           }
+           else
+           {
+               return true;
+           }
+       }
+   }
+
+   //add a new transform
+   m_transforms.push_back(new_tr);
+   return true;
 }
 
 bool FrameTransformContainer::deleteTransform(std::string t1, std::string t2)
 {
-    std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
-    if (t1 == "*" && t2 == "*")
-    {
-        m_transforms.clear();
-        return true;
-    }
-    else
-    {
-        if (t1 == "*")
-        {
-            for (size_t i = 0; i < m_transforms.size(); )
-            {
-                //source frame is jolly, thus delete all frames with destination == t2
-                if (m_transforms[i].dst_frame_id == t2)
-                {
-                    m_transforms.erase(m_transforms.begin() + i);
-                    i = 0; //the erase operation invalidates the iteration, loop restart is required
-                }
-                else
-                {
-                    i++;
-                }
-            }
-            return true;
-        }
-        else
-        {
-            if (t2 == "*")
-            {
-                for (size_t i = 0; i < m_transforms.size(); )
-                {
-                    //destination frame is jolly, thus delete all frames with source == t1
-                    if (m_transforms[i].src_frame_id == t1)
-                    {
-                        m_transforms.erase(m_transforms.begin() + i);
-                        i = 0; //the erase operation invalidates the iteration, loop restart is required
-                    }
-                    else
-                    {
-                        i++;
-                    }
-                }
-                return true;
-            }
-            else
-            {
-                for (size_t i = 0; i < m_transforms.size(); i++)
-                {
-                    if ((m_transforms[i].dst_frame_id == t1 && m_transforms[i].src_frame_id == t2) ||
-                        (m_transforms[i].dst_frame_id == t2 && m_transforms[i].src_frame_id == t1))
-                    {
-                        m_transforms.erase(m_transforms.begin() + i);
-                        return true;
-                    }
-                }
-            }
-        }
-    }
+   std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
+   if (t1 == "*" && t2 == "*")
+   {
+       for (size_t i = 0; i < m_transforms.size(); i++)
+       {
+           invalidateTransform(m_transforms[i]);
+       }
+       return true;
+   }
+   else
+   {
+       if (t1 == "*")
+       {
+           for (size_t i = 0; i < m_transforms.size(); i++)
+           {
+               //source frame is jolly, thus delete all frames with destination == t2
+               if (m_transforms[i].dst_frame_id == t2)
+               {
+                   invalidateTransform(m_transforms[i]);
+               }
+           }
+           return true;
+       }
+       else
+       {
+           if (t2 == "*")
+           {
+               for (size_t i = 0; i < m_transforms.size(); i++)
+               {
+                   //destination frame is jolly, thus delete all frames with source == t1
+                   if (m_transforms[i].src_frame_id == t1)
+                   {
+                       invalidateTransform(m_transforms[i]);
+                   }
+               }
+               return true;
+           }
+           else
+           {
+               for (size_t i = 0; i < m_transforms.size(); i++)
+               {
+                   if ((m_transforms[i].dst_frame_id == t1 && m_transforms[i].src_frame_id == t2) ||
+                       (m_transforms[i].dst_frame_id == t2 && m_transforms[i].src_frame_id == t1))
+                   {
+                       invalidateTransform(m_transforms[i]);
+                       return true;
+                   }
+               }
+           }
+       }
+   }
 
-    yCError(FRAMETRANSFORSTORAGE) << "Transformation deletion not successful";
-    return false;
+   yCError(FRAMETRANSFORMCONTAINER) << "Transformation deletion not successful";
+   return false;
 }
 
 bool FrameTransformContainer::clearAll()
 {
-    std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
-    m_transforms.clear();
-    return true;
+   std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
+   for (size_t i = 0; i < m_transforms.size(); i++)
+   {
+       invalidateTransform(m_transforms[i]);
+   }
+   return true;
 }
 
 bool FrameTransformContainer::checkAndRemoveExpired()
 {
-    std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
-    double curr_t = yarp::os::Time::now();
-    for (auto it= m_transforms.begin(); it!= m_transforms.end(); it++)
-    {
-        if (it->timestamp- curr_t >m_timeout &&
-            it->isStatic == false)
-        {
-            m_transforms.erase(it);
-            it = m_transforms.begin();
-        }
-    }
-    return true;
+   std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
+   double curr_t = yarp::os::Time::now();
+check_vector:
+   for (auto it= m_transforms.begin(); it!= m_transforms.end(); it++)
+   {
+       if (curr_t - it->timestamp > m_timeout &&
+           it->isStatic == false)
+       {
+           if (m_verbose_debug)
+           {
+               if (it->isValid())
+               {yCIDebug(FRAMETRANSFORMCONTAINER, m_name) << "At time" << std::to_string(curr_t)
+                                                             <<"Transform expired:" << it->src_frame_id << "->" << it->dst_frame_id << "with timestamp" << std::to_string(it->timestamp);}
+               else
+               {yCIDebug(FRAMETRANSFORMCONTAINER, m_name) << "At time" << std::to_string(curr_t)
+                                                             << "Invalid transform expired:" << it->src_frame_id << "->"<< it->dst_frame_id << "with timestamp" << std::to_string(it->timestamp);}
+           }
+           m_transforms.erase(it);
+           goto check_vector;
+       }
+   }
+   return true;
 }
 
 bool FrameTransformContainer::checkAndRemoveExpired() const
 {
-    return const_cast<FrameTransformContainer*>(this)->checkAndRemoveExpired();
+   return const_cast<FrameTransformContainer*>(this)->checkAndRemoveExpired();
 }
 
 bool FrameTransformContainer::size(size_t& size) const
 {
-    std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
-    size = m_transforms.size();
-    return true;
+   std::lock_guard<std::recursive_mutex> lock(m_trf_mutex);
+   size = m_transforms.size();
+   return true;
 }
-
-/*
-bool FrameTransformStorage::delete_transform(int id)
-{
-    std::lock_guard<std::mutex> lock(m_mutex);
-    if (id >= 0 && (size_t)id < m_transforms.size())
-    {
-        m_transforms.erase(m_transforms.begin() + id);
-        return true;
-    }
-    return false;
-}
-
-
-yarp::math::FrameTransform& operator[]   (std::size_t idx)
-{
-    return m_transforms[idx];
-}
-*/
--- src/devices/frameTransformUtils/FrameTransformContainer.h
+++ src/devices/frameTransformUtils/FrameTransformContainer.h
@@ -1,7 +1,7 @@
 /*
- * SPDX-FileCopyrightText: 2006-2021 Istituto Italiano di Tecnologia (IIT)
- * SPDX-License-Identifier: LGPL-2.1-or-later
- */
+* SPDX-FileCopyrightText: 2006-2021 Istituto Italiano di Tecnologia (IIT)
+* SPDX-License-Identifier: LGPL-2.1-or-later
+*/
 
 #ifndef YARP_DEV_FRAMETRANSFORM_UTILS_H
 #define YARP_DEV_FRAMETRANSFORM_UTILS_H
@@ -17,56 +17,107 @@
 #include <map>
 
 /**
- *  @ingroup dev_impl_other
- *
- * \brief `FrameTransformContainer`: A class that contains a vector of frame transformations and exposes
- *  \ref yarp::dev::IFrameTransformStorageSet and \ref yarp::dev::IFrameTransformStorageGet
- *  interfaces in order to allow external access to it.
- *
- */
+*  @ingroup dev_impl_other
+*
+* \brief `FrameTransformContainer`: A class that contains a vector of frame transformations and exposes
+*  \ref yarp::dev::IFrameTransformStorageSet and \ref yarp::dev::IFrameTransformStorageGet
+*  interfaces in order to allow external access to it.
+*
+*/
 
 class FrameTransformContainer :
-    public yarp::dev::IFrameTransformStorageSet,
-    public yarp::dev::IFrameTransformStorageGet
+       public yarp::dev::IFrameTransformStorageSet,
+       public yarp::dev::IFrameTransformStorageGet
 {
-    using ContainerType = std::vector<yarp::math::FrameTransform>;
+   using ContainerType = std::vector<yarp::math::FrameTransform>;
+
+public:
+   struct Iterator
+   {
+       using iterator_category = std::forward_iterator_tag;
+       using difference_type = ContainerType::difference_type;
+       using value_type = ContainerType::value_type;
+       using pointer = ContainerType::iterator;
+       using reference = value_type&;
+
+       //constructor
+       Iterator(ContainerType& data, ContainerType::iterator ptr) :
+               m_data(data),
+               m_ptr(ptr)
+       {
+           while (m_ptr != m_data.end() && !m_ptr->isValid()) {
+               ++m_ptr;
+           }
+       }
+
+       reference operator*() const { return *m_ptr; }
+       pointer operator->() { return m_ptr; }
+
+       // Prefix increment
+       Iterator& operator++()
+       {
+           do {
+               ++m_ptr;
+           } while (m_ptr != m_data.end() && !m_ptr->isValid());
+           return *this;
+       }
+
+       // Postfix increment
+       Iterator operator++(int) { Iterator tmp = *this; ++(*this); return tmp; }
+
+       friend bool operator== (const Iterator& a, const Iterator& b) { return a.m_ptr == b.m_ptr; };
+       friend bool operator!= (const Iterator& a, const Iterator& b) { return a.m_ptr != b.m_ptr; };
+
+   private:
+       ContainerType::iterator m_ptr;
+       ContainerType& m_data;
+   };
+
+public:
+   Iterator begin()
+   {
+       return Iterator(m_transforms, m_transforms.begin());
+   }
+
+   Iterator end()
+   {
+       return Iterator(m_transforms, m_transforms.end());
+   }
 
 protected:
-    ContainerType m_transforms;
+   ContainerType m_transforms;
+   void invalidateTransform(yarp::math::FrameTransform& trf);
 
 public:
-    mutable std::recursive_mutex  m_trf_mutex;
+   mutable std::recursive_mutex  m_trf_mutex;
 
 public:
-    //non-static transforms older than value (seconds) will be removed by method checkAndRemoveExpired()
-    double m_timeout = 0.2;
+   //non-static transforms older than value (seconds) will be removed by method checkAndRemoveExpired()
+   double m_timeout = 0.2;
+   bool   m_verbose_debug = false;
+   std::string m_name;
 
 public:
-    FrameTransformContainer() {}
-    ~FrameTransformContainer() {}
+   FrameTransformContainer() {}
+   ~FrameTransformContainer() {}
 
-    //IFrameTransformStorageSet interface
-    bool setTransforms(const std::vector<yarp::math::FrameTransform>& transforms) override;
-    bool setTransform(const yarp::math::FrameTransform& transform) override;
+   //IFrameTransformStorageSet interface
+   bool setTransforms(const std::vector<yarp::math::FrameTransform>& transforms) override;
+   bool setTransform(const yarp::math::FrameTransform& transform) override;
 
-    //IFrameTransformStorageGet interface
-    bool getTransforms(std::vector<yarp::math::FrameTransform>& transforms) const override;
+   //IFrameTransformStorageGet interface
+   bool getTransforms(std::vector<yarp::math::FrameTransform>& transforms) const override;
 
-    //IFrameTransformStorageUtils interface
-    bool deleteTransform(std::string t1, std::string t2) override;
-    bool clearAll() override;
+   //IFrameTransformStorageUtils interface
+   bool deleteTransform(std::string t1, std::string t2) override;
+   bool clearAll() override;
 
-    bool size(size_t& size) const;
+   bool size(size_t& size) const;
 
 public:
-    //other
-    bool checkAndRemoveExpired();
-    bool checkAndRemoveExpired() const;
-    ContainerType::iterator begin() {return m_transforms.begin();}
-    ContainerType::iterator end()   {return m_transforms.end();}
-
-    //yarp::math::FrameTransform& operator[]   (std::size_t idx) { return m_transforms[idx]; }
-    //bool     delete_transform(int id);
+   //other
+   bool checkAndRemoveExpired();
+   bool checkAndRemoveExpired() const;
 };
 
 #endif // YARP_DEV_FRAMETRANSFORM_UTILS_H
