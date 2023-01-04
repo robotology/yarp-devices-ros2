811aff908a0f2536618ac7cb1a631d022ea8aef6
Marco Randazzo <marco.randazzo@iit.it>
2023-01-04 17:29:57 +0100

    added harness files for tests witch catch2

---
+++ tests/harness.h
@@ -0,0 +1,35 @@
+/*
+ * SPDX-FileCopyrightText: 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * SPDX-FileCopyrightText: 2006-2010 RobotCub Consortium
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef YARP_TESTS_HARNESS_H
+#define YARP_TESTS_HARNESS_H
+
+#if !defined(WITHOUT_NETWORK)
+#  include <yarp/os/YarpPluginSelector.h>
+#endif // WITHOUT_NETWORK
+
+#include <iostream>
+#include <catch2/catch_amalgamated.hpp>
+
+void increment_tests_skipped();
+
+#define YARP_SKIP_TEST(...) \
+{ \
+    increment_tests_skipped(); \
+    FAIL(__VA_ARGS__); \
+}
+
+#if !defined(WITHOUT_NETWORK)
+#define YARP_REQUIRE_PLUGIN(name, type) \
+{ \
+    bool has_plugin = yarp::os::YarpPluginSelector::checkPlugin(name, type); \
+    if (!has_plugin) { \
+        YARP_SKIP_TEST("Required plugin is missing: " << type << " - " << name); \
+    } \
+}
+#endif // WITHOUT_NETWORK
+
+#endif // YARP_TESTS_HARNESS_H
