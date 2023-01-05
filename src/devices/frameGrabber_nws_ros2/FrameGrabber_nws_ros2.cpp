cd9b31daeb9b42fcef20769bb5df6b6242c5c5df
Marco Randazzo <marco.randazzo@iit.it>
2023-01-05 18:55:25 +0100

    license fix: year update

--- src/devices/frameGrabber_nws_ros2/FrameGrabber_nws_ros2.cpp
+++ src/devices/frameGrabber_nws_ros2/FrameGrabber_nws_ros2.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2021 Istituto Italiano di Tecnologia (IIT)
+ * Copyright (C) 2006-2023 Istituto Italiano di Tecnologia (IIT)
  * Copyright (C) 2006-2010 RobotCub Consortium
  * All rights reserved.
  *
