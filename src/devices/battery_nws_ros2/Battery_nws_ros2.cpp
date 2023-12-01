3cb21ed94b401733e579dfad333b071a6a63d462
fbrand-new <francesco.brand@iit.it>
2023-12-01 12:49:21 +0100

    Actual sonar code smell

--- src/devices/battery_nws_ros2/Battery_nws_ros2.cpp
+++ src/devices/battery_nws_ros2/Battery_nws_ros2.cpp
@@ -23,7 +23,7 @@ Battery_nws_ros2::~Battery_nws_ros2()
 
 bool Battery_nws_ros2::attach(yarp::dev::PolyDriver* driver)
 {
-    int a;
+    const int& a = 42;
     if (driver->isValid())
     {
         driver->view(m_battery_interface);
